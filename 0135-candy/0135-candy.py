ce9c3e08683b9ea9533bb3bce4c0420c7b02c35f
adityaS-123 <126447180+adityaS-123@users.noreply.github.com>
2024-08-28 20:41:38 +0530

    Time: 111 ms (50.80%), Space: 13.8 MB (15.97%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        if n == 0:
+            return 0
+        
+        # Initialize candies list with 1 candy for each child
+        candies = [1] * n
+        
+        # First pass: left to right
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        
+        # Second pass: right to left
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+        
+        # The minimum candies needed is the sum of the candies array
+        return sum(candies)
