4a46728d225ce7d8240b0e18fc8847f8d6cae44a
adityaS-123 <126447180+adityaS-123@users.noreply.github.com>
2024-09-15 00:07:45 +0530

    Time: 994 ms (5.07%), Space: 22.6 MB (46.64%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        num_map = {}
+        
+        for i, num in enumerate(nums):
+            if num in num_map and i - num_map[num] <= k:
+                return True
+            num_map[num] = i
+        
+        return False
